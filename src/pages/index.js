6960799212177831acd807be5631f43e0f0c950b
celine-fucci <Celine@Celines-MacBook-Pro.local>
2019-10-22 20:13:24 -0400

    build fix #4

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,23 +3,15 @@ import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import NavWrapper from "./../components/nav"
 
-//import NavWrapper from "../components/nav"
+// TODO
+import Nav from "../components/nav"
 
-const NavLine = styled.hr`
-  margin-top: 21px;
-  max-width: 100%;
-  height: 1px;
-  border: 0;
-  background-color: var(--blue-grey-eight);
-`
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <NavWrapper />
-    <NavLine />
+    <Nav />
   </Layout>
 )
 
