c1bbfede771a11c6270a2757076777499cfb5b30
celine-fucci <Celine@Celines-MacBook-Pro.local>
2019-11-22 17:06:26 -0500

    form test

--- src/components/contactform.js
+++ src/components/contactform.js
@@ -3,54 +3,68 @@ import styled from "styled-components"
 import { Link } from "gatsby"
 import "../styles/palette.scss"
 
-const Contactform = () => (
-    <form 
-        name="contact" 
-        method="post"
-        action="/success"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-    >
-        <input type="hidden" name="bot-field" />
+const ContactForm = () => (
+    // <form 
+    //     name="contact" 
+    //     method="post"
+    //     action="/success"
+    //     data-netlify="true"
+    //     data-netlify-honeypot="bot-field"
+    // >
+    //     <input type="hidden" name="bot-field" />
 
-        <div>
+    //     <div>
+    //         <label htmlFor="name">Name</label>
+    //         <input type="text" name="name" id="name" />
+    //     </div>
+
+    //     <div>
+    //         <label htmlFor="email">Email</label>
+    //         <input type="text" name="email" id="email" />
+    //     </div>
+
+    //     <div>
+    //         <label htmlFor="message">Message</label>
+    //         <textarea name="message" id="message" rows="6" required />
+    //     </div>
+
+    //     <div>
+    //         <input type="submit" value="Drop a line" />
+    //         <input type="reset" value="Eraser" />
+    //     </div>
+    // </form>
+
+
+    <form name="contact" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field"> 
+        <input type="hidden" name="bot-field" /> 
+        <input type="hidden" name="form-name" value="contact" />  
+
+        <div className="field half first">
             <label htmlFor="name">Name</label>
             <input type="text" name="name" id="name" />
         </div>
 
-        <div>
+        <div className="field half">
             <label htmlFor="email">Email</label>
             <input type="text" name="email" id="email" />
         </div>
 
-        <div>
+        <div className="field">
             <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" rows="6" required />
+            <textarea name="message" id="message" rows="6" />
         </div>
-
-        <div>
-            <input type="submit" value="Drop a line" />
-            <input type="reset" value="Eraser" />
-        </div>
+        
+        <ul className="actions">
+            <li>
+            <input type="submit" value="Send Message" className="special" />
+            </li>
+            <li>
+            <input type="reset" value="Clear" />
+            </li>
+        </ul>
     </form>
+
+
 )
 
-export default Contactform
-
-// const ContactWrapper = styled.div`
-
-//   @media (max-width: 768px) {
-//     min-height: 21vh;
-//   }
-// `
-
-
-// const FooterText = styled.footer`
-//   margin-top: auto;
-//   color: var(--blue-grey-six);
-//   font-size: 12px;
-//   font-weight: 300;
-// `
-
-
-//export default Contact
+export default ContactForm
